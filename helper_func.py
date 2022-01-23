6ca1446e809fd7403d3d505f06ee51b1b84d5f0a
MasterParrel <97712767+MasterParrel@users.noreply.github.com>
2022-01-24 03:33:10 +0700

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -16,7 +16,7 @@ from config import ADMINS, FORCE_SUB_CHANNEL, FORCE_SUB_GROUP
 async def is_subscribed(filter, client, update):
     if not FORCE_SUB_CHANNEL:
         retrun True
-    if not FORCE_SUB_CHANNEL:
+    if not FORCE_SUB_CHANNEL2:
         return True
     if not FORCE_SUB_GROUP:
         return True
